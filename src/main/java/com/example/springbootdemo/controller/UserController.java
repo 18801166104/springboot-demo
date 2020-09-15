d2494126856d9edf06ddc9874a846f5e9869a2cf
curry.wang <18801166104@163.com>
2020-09-15 21:54:47 +0800

    新建gitdemo文件类

---
+++ src/main/java/com/example/springbootdemo/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.springbootdemo.controller;
+
+/**
+ * @description:
+ * @author: curry.wang
+ * @create: 2020/9/15 18:15
+ **/
+public class UserController {
+}
---
+++ src/test/java/com/example/springbootdemo/gitdemo.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemo;
+
+/**
+ * @description:
+ * @author: curry.wang
+ * @create: 2020/9/15 21:48
+ **/
+public class gitdemo {
+    public static void main(String[] args) {
+        System.out.println("gitdemo");
+    }
+}
